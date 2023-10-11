3eeb2f4e0b0fc642d05def5a21937620bc8d722e
selfcan <99958756+selfcan@users.noreply.github.com>
2023-10-11 08:05:27 +0800

    Update autostart.sh

--- trunk/user/scripts/autostart.sh
+++ trunk/user/scripts/autostart.sh
@@ -96,3 +96,11 @@ if [ $(nvram get frpc_enable) = 1 ] ; then
 logger -t "自动启动" "正在启动frp client"
 /usr/bin/frp.sh start
 fi
+if [ $(nvram get alist_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动alist"
+/usr/bin/alist.sh start
+fi
+if [ $(nvram get tracker_enable) = 1 ] ; then
+logger -t "自动启动" "正在更新tracker"
+/usr/bin/tracker.sh start
+fi
